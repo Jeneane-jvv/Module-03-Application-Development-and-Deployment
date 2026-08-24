28d842b2cf75ccd746b17e7f71be369daec5c7f5
Jeneane van Vuuren <ninadieskim@gmail.com>
2026-08-25 01:21:22 +0200

    test: align frontend regression tests with application

--- frontend/src/app/pages/login/login.spec.ts
+++ frontend/src/app/pages/login/login.spec.ts
@@ -1,4 +1,7 @@
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
 import { Login } from './login';
 
 describe('Login', () => {
@@ -8,6 +11,11 @@ describe('Login', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [Login],
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting(),
+        provideRouter([]),
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(Login);
--- frontend/src/app/pages/mission-workspace/mission-workspace.spec.ts
+++ frontend/src/app/pages/mission-workspace/mission-workspace.spec.ts
@@ -1,4 +1,7 @@
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
 import { MissionWorkspace } from './mission-workspace';
 
 describe('MissionWorkspace', () => {
@@ -8,6 +11,11 @@ describe('MissionWorkspace', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [MissionWorkspace],
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting(),
+        provideRouter([]),
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(MissionWorkspace);
